e1f69aea044cf8574d3ccd59118d01691214711b
VishalNandy17 <nandyvishal17@gmail.com>
2025-08-31 23:38:41 +0530

    Fix: TypeScript signer error in BlockchainService

--- services/api/src/services/blockchain.ts
+++ services/api/src/services/blockchain.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7cb4b5acb1117203a3920a7ec95013c051f4c5e3c54b38cbe7be3e73bbd90b64
-size 12918
+oid sha256:79c7d7e6743262a5febdce92a6a558435586f96670db33d7e505f0f49cee71c9
+size 12921
